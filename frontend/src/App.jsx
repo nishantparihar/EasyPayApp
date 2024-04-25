2bfc21db579c4b0f07a20e7459d199cf42cd238b
nishantparihar <nishprihar@gmail.com>
2024-04-25 23:01:01 +0530

    started adding components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+import { Signin } from "./component/Signin";
+import { Signup } from "./component/Signup";
+import { Dashboard } from "./component/Dashboard";
+import { SendMoney } from "./component/SendMoney";
 
 function App() {
 
   return (
     <div>
-        Hello world
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/" element={<Signup />} />
+                <Route path="/dashboard" element={ <Dashboard />} />
+                <Route path="/send" element={ <SendMoney />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
