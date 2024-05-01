08527f5a385f09972021de388bda23cf8f050161
nishantparihar <nishprihar@gmail.com>
2024-05-02 01:15:11 +0530

    vercel backnend issue

--- frontend/src/assets/server.js
+++ frontend/src/assets/server.js
@@ -1,2 +1,2 @@
-export const server = "https://easypayapp.onrender.com/api/v1/";
-// export const server = "https://easypaybackend-mvqmt9whk-nishant-singh-parihars-projects.vercel.app/api/v1/";
+// export const server = "https://easypayapp.onrender.com/api/v1/";
+export const server = "https://easypaybackend-2kcv4zqfg-nishant-singh-parihars-projects.vercel.app/api/v1/";
