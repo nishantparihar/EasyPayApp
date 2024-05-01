b0e58bf459dcba1b73f1bfc7dcdd3ce4e94d63d8
nishantparihar <nishprihar@gmail.com>
2024-05-02 01:18:39 +0530

    that's end of vercel backend issue. I lost

--- frontend/src/assets/server.js
+++ frontend/src/assets/server.js
@@ -1,2 +1,1 @@
-// export const server = "https://easypayapp.onrender.com/api/v1/";
-export const server = "https://easypaybackend-2kcv4zqfg-nishant-singh-parihars-projects.vercel.app/api/v1/";
+export const server = "https://easypayapp.onrender.com/api/v1/";
