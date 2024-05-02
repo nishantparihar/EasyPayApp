fecfc84498406471b1746fb6b49a6ce88d02b3a7
nishantparihar <nishprihar@gmail.com>
2024-05-03 00:09:56 +0530

    sign up error pop up fixed

--- frontend/src/assets/server.js
+++ frontend/src/assets/server.js
@@ -1,1 +1,2 @@
-export const server = "https://easypayapp.onrender.com/api/v1/";
+// export const server = "https://easypayapp.onrender.com/api/v1/";
+export const server = "http://localhost:3000/api/v1/";
